796bc47d0986c091b99ab3d8e20f7e5ea6f642fa
INADA Naoki <inada-n@klab.com>
2012-09-26 22:31:48 +0900

    myprofiler ライクなコマンドラインオプションに対応.

--- delete_old_log.py
+++ delete_old_log.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 import datetime as dt
-import os
+import os, re
 import sys
 import time
 import logging
 from ConfigParser import SafeConfigParser
-import optparse
+from optparse import OptionParser
 
 try:
     import MySQLdb
@@ -18,29 +18,70 @@ except ImportError:
         sys.exit(1)
 
 
-def connect(conf='~/.my.cnf', section='batch'):
+class NoValueConfigParser(SafeConfigParser):
     """
-    connect to MySQL from conf file.
+    ConfigParser accepts no value.
     """
-    parser = SafeConfigParser()
-    parser.read([os.path.expanduser(conf)])
-    config = dict(parser.items(section))
-    params = {}
-
-    for n in ('host', 'user', 'db'):
-        if n in config:
-            params[n] = config[n]
-
-    if 'port' in config:
-        params['port'] = int(config['port'])
-
-    if 'password' in config:
-        params['passwd'] = config['password']
-
-    return MySQLdb.connect(**params)
+    OPTCRE = re.compile(
+        r'(?P<option>[^:=\s][^:=]*)'          # very permissive!
+        r'\s*'                                # any number of space/tab,
+        r'(?P<vi>[:=]?)\s*'                   # optionally followed by
+                                              # separator (either : or
+                                              # =), followed by any #
+                                              # space/tab
+        r'(?P<value>.*)$'                     # everything up to eol
+        )
 
 
-def delete_old_log(con, tablename, days, blocksize=1000):
+def connect(cnf):
+    args = {}
+    args['host'] = cnf.get('host', 'localhost')
+    args['user'] = cnf.get('user', '')
+    args['passwd'] = cnf.get('password', '')
+    args['charset'] = cnf.get('default-character-set', 'utf8')
+    if 'port' in cnf:
+        args['port'] = int(cnf.get('port'))
+    if 'db' in cnf:
+        args['db'] = cnf['db']
+    return MySQLdb.connect(**args)
+
+def read_mycnf(extra_file=None, group_suffix=''):
+    cnf_files = ['/etc/my.cnf']
+    if extra_file is not None:
+        if not os.path.isfile(extra_file):
+            print >>sys.stderr, "[warn]", extra_file, "is not exists."
+        else:
+            cnf_files += [extra_file]
+    cnf_files += ['~/.my.cnf']
+    cnf_files = map(os.path.expanduser, cnf_files)
+
+    parser = NoValueConfigParser()
+    parser.read(cnf_files)
+
+    cnf = dict(parser.items('client'))
+    if group_suffix:
+        cnf.update(parser.items('client' + group_suffix))
+    return cnf
+
+
+def build_option_parser(usage='%prog [options]'):
+    parser = OptionParser(add_help_option=False, usage=usage)
+    parser.add_option(
+            '-e', '--defaults-extra-file', dest='extra_file',
+            help="Read MySQL configuration from this file additionaly",
+            )
+    parser.add_option(
+            '-s', '--defaults-group-suffix', dest='group_suffix',
+            help="Read MySQL configuration from this section additionally",
+            )
+    parser.add_option('-u', '--user')
+    parser.add_option('-p', '--password')
+    parser.add_option('-h', '--host')
+    parser.add_option('-?', '--help', action="store_true", help="show this message")
+    return parser
+
+
+def delete_old_log(con, tablename, days, blocksize=100, dry=False):
     u"""ログテーブルから古いログを消す.
 
     MySQL への接続 *con* を利用し、 *tablename* で指定された
@@ -79,7 +120,10 @@ def delete_old_log(con, tablename, days, blocksize=1000):
         q = "DELETE LOW_PRIORITY FROM %s WHERE `id`<=%d" % (tablename, e)
         logging.debug(q)
         t = time.time()
-        cur.execute(q)
+        if dry:
+            time.sleep(0.05)
+        else:
+            cur.execute(q)
         t = time.time() - t
         if t < 0.01 and not breaking:
             blocksize = int(blocksize * 1.1) +1
@@ -92,26 +136,46 @@ def delete_old_log(con, tablename, days, blocksize=1000):
 
 
 def main():
-    parser = optparse.OptionParser("%prog [OPTIONS] tablename")
+    parser = build_option_parser('%prog [options] db_name table_name_1 [table_name_2...]')
     parser.add_option('-d', '--days', type='int', default=30)
-    parser.add_option('-s', '--section', type='string', action="store", default='batch')
-    parser.add_option('-b', '--blocksize', type='int', default=500)
+    parser.add_option('-b', '--blocksize', type='int', default=100)
+    parser.add_option('--dry', action='store_const', const=1, default=0)
     parser.add_option('-v', '--verbose', action='store_const', const=1, default=0)
 
     opts, args = parser.parse_args()
-    if not args:
-        parser.error("Only one tablename should be specified.")
+    outfile = None
+
+    if opts.help:
+        parser.print_help()
+        return
+
+    if len(args) < 2:
+        parser.error('args is required.')
+        return
+
+    try:
+        cnf = read_mycnf(opts.extra_file, opts.group_suffix)
+        if opts.user:
+            cnf['user'] = opts.user
+        if opts.password:
+            cnf['password'] = opts.password
+        if opts.host:
+            cnf['host'] = opts.host
+        cnf['db'] = args[0]
+        con = connect(cnf)
+    except Exception, e:
+        parser.error(e)
+        return
 
     loglevel = logging.DEBUG if opts.verbose else logging.INFO
     logging.basicConfig(level=loglevel,
                         format="%(asctime)s %(levelname)-7s: %(message)s",
                         )
 
-    con = connect(section=opts.section)
     con.autocommit(True)
-    for table in args:
+    for table in args[1:]:
         logging.info("Start deleting old logs from: %s", table)
-        delete_old_log(con, table, opts.days)
+        delete_old_log(con, table, opts.days, opts.blocksize, dry=opts.dry)
 
 if __name__ == '__main__':
     main()
